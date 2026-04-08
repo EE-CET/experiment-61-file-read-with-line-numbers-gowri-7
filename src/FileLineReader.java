f3fd688c2f8fab10b0aa1cfbb0a7bea343b15ec1
gowri-7 <166044015+gowri-7@users.noreply.github.com>
2026-04-08 12:02:22 +0530

    Add file reading functionality with line numbers
    Implemented file reading with line numbering and error handling.

--- src/FileLineReader.java
+++ src/FileLineReader.java
@@ -6,12 +6,21 @@ public class FileLineReader {
     public static void main(String[] args) {
         String fileName = "input.txt";
         
-        // TODO: Use a try-with-resources block to open a BufferedReader for the file
-        // TODO: Read the file line by line using a loop
-        // TODO: Keep track of the line number (starting from 1)
-        // TODO: Print each line in the format "lineNumber: lineContent"
-        // TODO: Catch and handle any IOExceptions that might occur
-        
-        
+        // try-with-resources
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            
+            String line;
+            int lineNumber = 1;
+            
+            // Read line by line
+            while ((line = br.readLine()) != null) {
+                System.out.println(lineNumber + ": " + line);
+                lineNumber++;
+            }
+            
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+        }
     }
 }
+
